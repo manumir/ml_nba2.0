ea7a54848807649790b819040b166af079f838c2
space <miranda.junior@live.com.pt>
2020-02-23 17:38:04 +0000

    a

--- scraper.py
+++ scraper.py
@@ -17,33 +17,39 @@ currentpath=os.getcwd()
 path2data=os.path.join(currentpath,"data\\")
 
 # initiate driver
-driver = webdriver.Chrome(executable_path='C:/Users/dude/Desktop/chromedriver.exe')
-driver.get('https://stats.nba.com/gamebooks/?Date=12%2F30%2F2019')# day of games
-driver.get('https://stats.nba.com/game/0021801220/')# example game page
+#driver = webdriver.Chrome(executable_path='C:/Users/dude/Desktop/chromedriver.exe')
+driver = webdriver.Firefox(executable_path='../geckodriver')
+#driver.get('https://stats.nba.com/gamebooks/?Date=12%2F30%2F2019')# day of games
+for i in range(1300):
+	driver.get('https://stats.nba.com/game/0021801220/')# example game page
 
-# wait till page loads
-WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.nba-stat-table__caption")))
+	# wait till page loads
+	WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.nba-stat-table__caption")))
 
-# find div of the games
-soup=bs4(driver.page_source,'html.parser')
-games=soup.find_all('div',class_="nba-stat-table")
+	# find div of the games
+	soup=bs4(driver.page_source,'html.parser')
+	games=soup.find_all('div',class_="nba-stat-table")
 
-games=bs4(str(games[0]),'html.parser')
-#print(games.div.table.tbody.text)
-links = []
+	games=bs4(str(games[0]),'html.parser')
+	#print(games.div.table.tbody.text)
+	links = []
 
-for link in games.find_all('a', attrs={'href': re.compile("^/game")}):
-    links.append(link.get('href'))
+	for link in games.find_all('a', attrs={'href': re.compile("^/game")}):
+		links.append(link.get('href'))
 
-print(links)
-#features = html.table.thead.tr.text #don't need to scrape this multiple times
+	print(links)
+	#features = html.table.thead.tr.text #don't need to scrape this multiple times
 
-stuff=soup.find_all('div',class_="nba-stat-table__overflow")
-for x in stuff:
-	print(x.text)
-#stats=html.table.tbody.text
-#path=driver.find_element_by_class_name("stats-table-pagination__next")
-#path.click()
-
-driver.quit()
-#print("scraped in %s seconds" % (time.time() - start_time))
+	stuff=soup.find_all('div',class_="nba-stat-table__overflow")
+	for x in stuff:
+		x=str(x.text)[92:]
+		x=x.replace("  ","")
+		x=x.replace("\n"," ")
+		print(x)
+	#stats=html.table.tbody.text
+	#path=driver.find_element_by_class_name("stats-table-pagination__next")
+	#path.click()
+	match=re.match(r'[A-Z]',x)
+	print(match)
+	driver.quit()
+	#print("scraped in %s seconds" % (time.time() - start_time))
