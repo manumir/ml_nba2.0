93be427f1db44f77ae24a0e850c1f2e58af69e5e
space <miranda.junior@live.com.pt>
2020-03-01 18:16:06 +0000

    a

--- scraper.py
+++ scraper.py
@@ -10,7 +10,6 @@ import re
 #driver = webdriver.Chrome(executable_path='C:/Users/dude/Desktop/chromedriver.exe')
 driver = webdriver.Firefox(executable_path='../geckodriver')
 #driver.get('https://stats.nba.com/gamebooks/?Date=12%2F30%2F2019')# day of games
-#driver.get('https://stats.nba.com/game/0021801220/')# example game page
 f=open('data.txt','w')
 gudstuff=[]
 for i in range(2):
@@ -19,6 +18,16 @@ for i in range(2):
 
 	time.sleep(7)# seconds
 	soup=bs4(driver.page_source,'html.parser')
+	nplaying=soup.find_all('div',class_="game-summary__inactive")
+	for x in nplaying:
+		x=x.get_text()
+		abc=x.split('\n')
+	ab=[]
+	for x in abc:
+		if x != '':
+			x=x.strip('  ')
+			ab.append(x)
+	print(ab)
 	stuff=soup.find_all('div',class_="nba-stat-table__overflow")
 	teams_soup=soup.find_all('div',class_="nba-stat-table__caption")
 	teams=[]
@@ -36,15 +45,10 @@ for i in range(2):
 		stop=0
 		end=0
 		while stop==0:
-			match2=re.search('DNP',x)
-			if match2:
-				print(match2)
-				x=x[:match2.end()]+','+x[match2.end()+1:]
-
 			match=re.search(r'[A-Z]',x[end:])
 			if match:
-				x=x[:match.start()+end-1]+'\n'+x[match.start()+end:]
-				end=match.end()+24+end
+				x=x[:match.start()+end]+'\n'+x[match.start()+end:]
+				end=match.end()+48+end
 			else:
 				x=x.replace("  "," ")
 				yumyum.append(x)
@@ -64,7 +68,8 @@ for i in range(2):
 		if x == '':
 			it=it+1
 		else:
-			f.write(teams[it]+','+x+'\n')
+			f.write(x+'\n')
+			#f.write(teams[it]+','+x+'\n')
 
 f.close()
 driver.quit()
