bfc6ea9cb08bfaab94235a924e91d9395992ac0d
space <miranda.junior@live.com.pt>
2020-05-17 00:47:28 +0100

    still need a lot of work

--- scraper.py
+++ scraper.py
@@ -11,14 +11,16 @@ import re
 driver = webdriver.Firefox(executable_path='../geckodriver')
 #driver.get('https://stats.nba.com/gamebooks/?Date=12%2F30%2F2019')# day of games
 f=open('data.txt','w')
-gudstuff=[]
+teams=[]
+it=-1
 for i in range(2):
-	it=-1
 	driver.get('https://stats.nba.com/game/002180'+str(i+1).zfill(4)+'/')
-
 	time.sleep(7)# seconds
 	soup=bs4(driver.page_source,'html.parser')
+
+	# get incative players
 	nplaying=soup.find_all('div',class_="game-summary__inactive")
+	# formatting incative players
 	for x in nplaying:
 		x=x.get_text()
 		abc=x.split('\n')
@@ -27,19 +29,43 @@ for i in range(2):
 		if x != '':
 			x=x.strip('  ')
 			ab.append(x)
-	print(ab)
+	ab=' '.join(ab)
+	teams1=['MEM','HOU','BKN','BOS','LAC','NOP','SAC','POR','DET','UTA','CHA','SAS','WAS','TOR','DEN','MIL','ATL','GSW','DAL','ORL','PHI','NYK','LAL','CLE','OKC','MIN','CHI','MIA','PHX','IND']
+	lines=[]
+	for team in teams1:
+		match=re.search(team,ab)
+		if match:
+			lines.append(ab[22:match.start()-1])
+			lines.append(ab[match.start()+4:])
+	
+	inactive=[]
+	lines=lines[0:2]
+	for x in lines:
+		x=''.join(x)
+		x=x.replace(', ',',')
+		x=x.split(',')
+		a=[]
+		for name in x:
+			a.append(name+',,,,,,')
+		inactive.append(a)
+
+	# get all that good data
 	stuff=soup.find_all('div',class_="nba-stat-table__overflow")
+	# get teams names
 	teams_soup=soup.find_all('div',class_="nba-stat-table__caption")
-	teams=[]
+	# get date of game
 	date=soup.find_all('div',class_="game-summary__date")
+	
+	# formatting teams
 	for team in teams_soup:
 		teams.append(team.get_text(strip=True)+','+date[0].get_text(strip=True))
+
+	# formatting data
 	yum=[]
 	for x in stuff:
 		x=str(x.get_text())[92:]
 		x=x.replace("\n"," ")
 		yum.append(x)
-
 	yumyum=[]
 	for x in yum:
 		stop=0
@@ -53,23 +79,37 @@ for i in range(2):
 				x=x.replace("  "," ")
 				yumyum.append(x)
 				stop=1
-
 	new=[]
 	for x in yumyum:
-		new=new+x.split('\n')
-
+		new.append(x.split('\n'))
 	for x in new:
-		x=' '.join(x.split())
+		for y in x:
+			y=' '.join(y.split())
+			match=re.search(r'[0-9]',y)
+			if match:
+				y=y[:match.start()-1]+','+y[match.start():].replace(' ',',')
+			
+			match=re.search('DNP',y)
+			if match:
+				y=y[:match.start()-1]+',,,,,,'
+			print(y)
+		
+	"""
+	if x == '':
+		it=it+1
+	else:
+		f.write(teams[it]+','+x+'\n')
 
-		match=re.search(r'[0-9]',x)
-		if match:
-			x=x[:match.start()-1]+','+x[match.start():].replace(' ',',')
-
-		if x == '':
-			it=it+1
-		else:
-			f.write(x+'\n')
-			#f.write(teams[it]+','+x+'\n')
-
+	print(teams[it]+','+x)
+for n in inactive:
+	for name in n:
+		print(teams[it]+','+name)
+	"""
 f.close()
 driver.quit()
+
+
+
+
+
+
